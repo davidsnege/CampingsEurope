440b0a4d0d3385551ab3cd06b33d889d73953b9d
David Belleti Snege <david.snege@gmail.com>
2021-02-22 23:40:21 +0100

    Update generalCampings.php

--- api/generalCampings.php
+++ api/generalCampings.php
@@ -41,16 +41,40 @@
 //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
 //║  Fazemos um loop foreach para recorrer a Array da variavel $result e printar na tela
 //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+
+echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">';
+
 //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
     // $count = 0;
     foreach ($result as $key => $value) {
-        echo '<pre>';
+        // echo '<pre>';
             if($value != ""){
                 // echo $count++;
-                print_r($value);
+                // print_r($value);
+            // };
+        // echo '</pre>';
+    // }
+
+// CARD TEST BOOTSTRAP
+
+        echo '<div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+        <div class="col-md-4">
+            <img width="200px" src="https://image.freepik.com/vector-gratis/iconos-camping-carpa-fogata-ilustracion_7130-1343.jpg" alt="'.$value['raisonSociale'].'">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+            <h5 class="card-title"> '.$value['raisonSociale'].'</h5>
+            <p class="card-text"> '.$value['addressOne'].'</p>
+            <p class="card-text">ID: '.$value['id'].' <small class="text-muted">'.$value['addressCountry'].'/'.$value['addressRegion'].'/'.$value['addressDepartement'].'</small></p>
+            </div>
+        </div>
+        </div>
+        </div>';
+
             };
-        echo '</pre>';
-    }
+        }
+
 //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
 //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
 //║  Encerramos a conexao com o mysql para nao consumir mais recursos
@@ -58,3 +82,5 @@
 //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
     $conecta->close();
 //╚══════════════════════════════════════════════════════════════════════════════════════════════════════════╝
+
+echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>';
