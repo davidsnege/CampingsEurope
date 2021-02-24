881bd23dc49fff26647423e7c700e854110b4c92
David Belleti Snege <david.snege@gmail.com>
2021-02-24 19:02:35 +0100

    Update generalCampings.php

--- myPHPScripts/generalCampings.php
+++ myPHPScripts/generalCampings.php
@@ -45,11 +45,11 @@
 echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">';
 
 //╔══════════════════════════════════════════════════════════════════════════════════════════════════════════╗
-    // $count = 0;
+    $count = 0;
     foreach ($result as $key => $value) {
         // echo '<pre>';
             if($value != ""){
-                // echo $count++;
+                echo $count++;
                 // print_r($value);
             // };
         // echo '</pre>';
