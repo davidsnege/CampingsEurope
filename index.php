1e3c2fcd85eefb886a8c59e0c81c98729f7cd4cf
David Belleti Snege <david.snege@gmail.com>
2021-03-12 21:38:31 +0100

    UI design

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 require 'public/head.html';
 require 'public/principal_head_bar.html';
 require 'public/body.html';
+require 'public/div1.html';
+require 'public/div2.html';
+// require 'public/div3.html';
 require 'public/footer.html';
 require 'public/script_footer.html';
